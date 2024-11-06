607c923698e7763c49688dae81011201828c0adb
MrWiggims1 <Liamth@live.com.au>
2024-11-06 20:35:34 +1000

    Improved FilterExtensions.cs

--- ZoneProductionLibrary/Extensions/FilterExtensions.cs
+++ ZoneProductionLibrary/Extensions/FilterExtensions.cs
@@ -3,6 +3,7 @@ using ZoneProductionLibrary.Models;
 using ZoneProductionLibrary.Models.Boards;
 using ZoneProductionLibrary.Models.Trello;
 using ZoneProductionLibrary.Models.ProductionPosition;
+using ZoneProductionLibrary.ProductionServices;
 
 namespace ZoneProductionLibrary.Extensions
 {
@@ -12,28 +13,20 @@ namespace ZoneProductionLibrary.Extensions
 
         public static IEnumerable<T> FilterCards<T>(this IEnumerable<T> cards, CardFilterOptions filterOptions) where T : IFilterableCard
         {
-            cards = cards.FilterCards(filterOptions.CardAreaOfOrigins);
-
-            if (filterOptions.SearchTerms != null)
-            {
-                cards = cards.FilterCards(filterOptions.SearchTerms, filterOptions.SearchExactName);
-            }
-
-            cards = cards.FilterCards(filterOptions.CardStatuses);
-
-            return cards;
-        }
-
-
-        public static IEnumerable<T> FilterCards<T>(this IEnumerable<T> cards, IEnumerable<string> searchTerms, bool SearchExactName) where T : IFilterableCard
-        {
-            if (SearchExactName)
-                cards.Where(x => searchTerms.Contains(x.Name));
-
-            else
-                cards = cards.Where(c => searchTerms.Any(s => CleanName(c.Name).Contains(s.ToLower())));
-
-            return cards;
+            return cards.Where(x => x is RedCard red
+                                        ?
+                                   (filterOptions.CardAreaOfOrigins.Contains(x.AreaOfOrigin) &&
+                                   filterOptions.CardStatuses.Contains(x.CardStatus) &&
+                                   filterOptions.RedFlagIssues.Contains(red.RedFlagIssue)) 
+                                        :
+                                    x is RedCardObject redO
+                                        ?
+                                        (filterOptions.CardAreaOfOrigins.Contains(x.AreaOfOrigin) &&
+                                         filterOptions.CardStatuses.Contains(x.CardStatus) &&
+                                         filterOptions.RedFlagIssues.Contains(redO.RedFlagIssue)) 
+                                        :
+                                   (filterOptions.CardAreaOfOrigins.Contains(x.AreaOfOrigin) &&
+                                    filterOptions.CardStatuses.Contains(x.CardStatus)));
         }
 
         public static IEnumerable<T> FilterCards<T>(this IEnumerable<T> cards, IEnumerable<CardStatus> cardStatuses) where T : IFilterableCard
@@ -44,15 +37,10 @@ namespace ZoneProductionLibrary.Extensions
 
         public static IEnumerable<T> FilterVans<T>(this IEnumerable<T> boards, BoardFilterOptions filterOptions) where T : IFilterableBoard
         {
-            boards = boards.FilterVans(filterOptions.vanTypes);
-
-            if (filterOptions.BoardIds != null)
-                boards = boards.FilterVans(filterOptions.BoardIds);
-
-            if (filterOptions.ProductionPositions != null)
-                boards = boards.FilterVans(filterOptions.ProductionPositions);
-
-            return boards;
+            return boards.Where(x =>
+                                    filterOptions.BoardIds.Contains(x.Id) &&
+                                    (!filterOptions.ProductionPositions.Any() || filterOptions.ProductionPositions.Contains(x.Position)) &&
+                                    (!filterOptions.vanTypes.Any() || filterOptions.vanTypes.Contains(x.VanModel)));
         }
 
         public static IEnumerable<T> FilterVans<T>(this IEnumerable<T> boards, IEnumerable<string> boardIds) where T : IFilterableBoard
@@ -69,16 +57,12 @@ namespace ZoneProductionLibrary.Extensions
     {
         public IEnumerable<CardAreaOfOrigin> CardAreaOfOrigins { get; set; }
         public IEnumerable<RedFlagIssue> RedFlagIssues { get; set; }
-        public IEnumerable<string>? SearchTerms { get; set; }
-        public bool SearchExactName { get; set; }
         public IEnumerable<CardStatus> CardStatuses { get; set; }
 
         public CardFilterOptions()
         {
             CardAreaOfOrigins = Enum.GetValues<CardAreaOfOrigin>();
             RedFlagIssues = Enum.GetValues<RedFlagIssue>();
-            SearchTerms = null;
-            SearchExactName = false;
             CardStatuses = Enum.GetValues<CardStatus>();
         }
     }
@@ -86,14 +70,14 @@ namespace ZoneProductionLibrary.Extensions
     public class BoardFilterOptions
     {
         public IEnumerable<VanModel> vanTypes { get; set; }
-        public IEnumerable<string>? BoardIds { get; set; }
-        public IEnumerable<IProductionPosition>? ProductionPositions { get; set; }
+        public IEnumerable<string> BoardIds { get; set; }
+        public IEnumerable<IProductionPosition> ProductionPositions { get; set; }
 
         public BoardFilterOptions()
         {
             vanTypes = Enum.GetValues<VanModel>().Where(x => x.IsGen2());
-            BoardIds = null;
-            ProductionPositions = null;
+            BoardIds = [];
+            ProductionPositions = [];
         }
     }
 }
